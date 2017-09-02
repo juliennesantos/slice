aea8b440e105a6d21062d1dac227a590f7adfe69
juliennesantos <julienne.santos@benilde.edu.ph>
2017-09-02 12:26:44 +0800

    init files
    init files plus xampp installer

---
+++ application/views/pages/about.php
@@ -0,0 +1,1 @@
+<h2>This is about you.</h2>
---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+</head>
+<body>
+    <h1>CodeIgniter 2 Tutorial</h1>
+</body>
+</html>
