8ffaa871b0d13261a959931820d550b083881152
Sean Agustin <seanmerwin.agustin@benilde.edu.ph>
2017-10-09 03:22:08 +0800

    Changed Login and dashboard
    Login: css changes
    Dash: html & css

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,90 +1,40 @@
 <div class="row">
-<div class="col-md-12">
-    <!-- PR -->
+<div class="col-lg-12">
+    <!--BOX1-->
     <div class="col-lg-4">
-        <div class="box box-primary box-solid">
-            <div class="box-header with-border">
-                <h3 class="box-title">
-                    Your Tutorial Requests
-                </h3>
-
-                <div class="box-tools pull-right">
-                    <span data-toggle="tooltip" title="1 approved tutorial request/s" class="badge bg-green">1</span>
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
-                                </button>
-                </div>
-                <!-- /.box-tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <ul class="nav nav-stacked">
-                    <li>Tutorial Requests <span class="pull-right badge bg-blue">1</span></li>
-                    <li>Pending Tutorial Requests <span class="pull-right badge bg-aqua">1</span></li>
-                    <li>Approved Tutorial Requests <span class="pull-right badge bg-green">1</span></li>
-                </ul>
-            </div>
-            <!-- /.box-footer -->
+        <div class="list-group">
+            <li class="list-group-item list-group-item-warning">Your Tutorial Requests</li>
+            <a href="#" class="list-group-item">Tutorial Requests<span class="badge">21</span></a>
+            <a href="#" class="list-group-item">Pending Tutorial Requests<span class="badge">21</span></a>
+            <a href="#" class="list-group-item">Approved Tutorial Requests<span class="badge">21</span></a>
         </div>
     </div>
-    <!-- / PR -->
+    <!--BOX1-->
 
-    <!-- Bids    -->
+    <!--BOX2-->
     <div class="col-lg-4">
-        <div class="box box-warning box-solid">
-            <div class="box-header with-border">
-                <h3 class="box-title">
-                    Feedback
-                </h3>
-
-                <div class="box-tools pull-right">
-                    <span data-toggle="tooltip" title="1 approved bids" class="badge bg-light-blue">1</span>
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
-                                </button>
-                </div>
-                <!-- /.box-tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <ul class="nav nav-stacked">
-                    <li>All Feedbacks <span class="pull-right badge bg-blue">1</span></li>
-                    <li>Pending Feedback <span class="pull-right badge bg-aqua">1</span></li>
-                    <li>Answered Feedback <span class="pull-right badge bg-green">1</span></li>
-                </ul>
-            </div>
-            <!-- /.box-body -->
+        <div class="list-group">
+            <li class="list-group-item list-group-item-warning">Feedback</li>
+            <a href="#" class="list-group-item">All Feedbacks<span class="badge">21</span></a>
+            <a href="#" class="list-group-item">Pending Feedbacks<span class="badge">21</span></a>
+            <a href="#" class="list-group-item">Answered Feedbacks<span class="badge">21</span></a>
         </div>
     </div>
-    <!-- / Bids -->
+    <!--BOX2-->
 
-    <?php if($_SESSION['typeID'] == 2 || $_SESSION['typeID'] == 3 || $_SESSION['typeID'] == 4):?>
-
-    <!-- PR Approval -->
+    <!--BOX2-->
     <div class="col-lg-4">
-        <div class="box box-primary box-solid">
-            <div class="box-header with-border">
-                <h3 class="box-title">
-                    Purchase Requests for Approval
-                </h3>
-
-                <div class="box-tools pull-right">
-                    <span data-toggle="tooltip" title="1 pending PRs" class="badge bg-light-blue">1</span>
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
-                                </button>
-                </div>
-                <!-- /.box-tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <ul class="nav nav-stacked">
-                    <li>Pending Purchase Requests <span class="pull-right badge bg-aqua">1</span></li>
-                    <li>Endorsed Purchase Requests <span class="pull-right badge bg-green"></span></li>
-                    <li>Approved Purchase Requests <span class="pull-right badge bg-green">1</span></li>
-                </ul>
-            </div>
-            <!-- /.box-body -->
+        <div class="list-group">
+            <li class="list-group-item list-group-item-warning">Your Tutorial Requests</li>
+            <a href="#" class="list-group-item">Tutorial Requests<span class="badge">21</span></a>
+            <a href="#" class="list-group-item">Pending Tutorial Requests<span class="badge">21</span></a>
+            <a href="#" class="list-group-item">Approved Tutorial Requests<span class="badge">21</span></a>
         </div>
     </div>
-    <!-- / PR Approval -->
+    <!--BOX2-->
+    
+ <?php if($_SESSION['typeID'] == 2 || $_SESSION['typeID'] == 3 || $_SESSION['typeID'] == 4):?>
+
     <?php endif;?>
 </div>
 </div>
--- application/views/login/login.php
+++ application/views/login/login.php
@@ -7,14 +7,14 @@
   <title>AdminLTE 2 | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+   <!-- Theme style-->
+   <link rel="stylesheet" href="<?php echo base_url(); ?>resources/css/AdminLTE.min.css">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>resources/css/AdminLTE.min.css">
   <!-- iCheck -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/iCheck/square/blue.css">
 
@@ -41,11 +41,11 @@
       <?php echo form_open('login/validate'); ?>
       <div class="form-group has-feedback">
         <input type="text" name="username" class="form-control" id="username" placeholder="Username" />
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <span class="ion-person form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
         <input type="password" name="password" class="form-control" id="password" placeholder="Password" />
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        <span class="ion-locked form-control-feedback"></span>
       </div>
       <!-- <div class="row">
         <div class="col-xs-8">
