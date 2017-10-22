71e70978757ad22d90807ec5ad10cd8fdedfa008
Sean Agustin <seanmerwin.agustin@benilde.edu.ph>
2017-10-23 02:27:56 +0800

    NEW LOGIN 70% done
    :crying_cat_face:

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -7,12 +7,13 @@
   <title>AdminLTE 2 | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-   <!-- Theme style-->
-   <link rel="stylesheet" href="<?php echo base_url(); ?>resources/css/AdminLTE.min.css">
+   
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Theme style-->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>resources/css/AdminLTE.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/Ionicons/css/ionicons.min.css">
   <!-- iCheck -->
@@ -29,53 +30,60 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 
-<body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href="<?php echo base_url(); ?>index2.html">SLIC<b>e</b></a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+<body id="loginbody">
+<div id="container">
+  <div id="one">
+     <h1 id="spaceleft">asdasd CAROUSEL HERE djhsasdksjahdjaksdajjjjjjjjjjjjjjjjjjjjjjjjdhsjjj2jjjl</h1>
+  </div>
+  <div id="two">
+  <div class="row">
+  <div class="col-lg-offset-7 col-lg-5" id="spacetop">
+  <a href="<?php echo site_url()?>tutorregister/new_tutor" class="btn btn-outline">New Member? Sign Up Here</a>
+  </div>
+  </div>
+    <div class="login-box">
+      <div class="login-box-body">
+        <?php echo form_open('login/validate'); ?>
 
-      <?php echo form_open('login/validate'); ?>
-      <div class="form-group has-feedback">
-        <input type="text" name="username" class="form-control" id="username" placeholder="Username" />
-        <span class="ion-person form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" name="password" class="form-control" id="password" placeholder="Password" />
-        <span class="ion-locked form-control-feedback"></span>
-      </div>
-      <!-- <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon" id="add"><i class="fa fa-user"></i></span>
+              <input type="text" name="username" class="fc" id="username" placeholder="Username" />
+            </div>
           </div>
-        </div>
-        /.col -->
-      <div class="row">
-        <div class="col-xs-12">
-          <button type="submit" class="btn btn-login1 btn-block" name='signin'>Sign In</button>
-          <a href="<?php echo site_url()?>tutorregister/new_tutor" class="btn btn-login2 btn-block">Register as a new tutor</a>
-          <a href="<?php echo site_url()?>tutorregister/old" class="btn btn-login3 btn-block">Register as an old tutor</a>
-        </div>
+          <br/>
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon" id="add"><i class="fa fa-key"></i></span>
+              <input type="password" name="password" class="fc" id="password" placeholder="Password" />
+            </div>
+          </div>
+
+          <br/><br/>
+
+            <div class="row">
+            <div class="col-xs-12">
+              <button type="submit" class="btn btn-login1 pull-left" name='signin'>Sign In</button>
+            </div><br/>
+
+        </form>
       </div>
-      <!-- /.col -->
-      </form><!--SAN SIMULA NG FORM-->
-      <!--div class="col-xs-12">
-        <a href="<#?php echo site_url()?>tutorregister/new" class="btn btn-login3 btn-block">Register as a new tutor</a><br>
-        <a href="<#?php echo site_url()?>tutorregister/old" class="text-center">Register as an old tutor</a>
-      </div-->
     </div>
-
+        
+    <footer class="footer">
+      <div class="container">
+      <table>
+        <tr>
+          <td class="col-lg-4"><h5>SLICE</h5></td>
+          <td class="pull-right"><h5>2017</h5></td>
+        </tr>
+  </table>
   </div>
-  <!-- /.login-box-body -->
+      </footer>
   </div>
-  <!-- /.login-box -->
+</div>
 
+  
   <!-- jQuery 3 -->
   <script src="<?php echo base_url(); ?>bower_components/jquery/dist/jquery.min.js"></script>
   <!-- Bootstrap 3.3.7 -->
