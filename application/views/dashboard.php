6ca0ea3fc71494a9b1c77079d439a2be0c9cd542
Sean Agustin <seanmerwin.agustin@benilde.edu.ph>
2017-11-01 01:10:20 +0800

    dashboard front not yet done

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,5 +1,3 @@
-<div class="row">
-<div class="col-lg-12">
     <!--tutee tutsess box-->
     <?php if($_SESSION['typeID'] == 1): ?>
     <div class="col-lg-6">
@@ -65,46 +63,136 @@
 
 
     <!--admin tutsess box-->
-    <?php if($_SESSION['typeID'] == 3 || $_SESSION['typeID'] == 5): ?>
-    <div class="col-lg-6">
-        <div class="list-group">
-            <li class="list-group-item list-group-item-warning"><?=$title1?></li>
-            <a href="#" class="list-group-item">All Tutorial Sessions<span class="badge">
-            <?php
-            if($_SESSION['typeID'] == 5){
-                echo $admin_sess;
-            }
-            ?>
-            </span></a>
-            <a href="#" class="list-group-item">Pending Sessions<span class="badge">
-            <?php
-            if($_SESSION['typeID'] == 5){
-                echo $admin_pend;
-            }
-            ?>
-            </span></a>
-            <a href="#" class="list-group-item">Finished Feedbacks<span class="badge">
-            <?php
-            if($_SESSION['typeID'] == 5){
-                echo $admin_feedback;
-            }
-            ?>
-            </span></a>
+<?php if($_SESSION['typeID'] == 3 || $_SESSION['typeID'] == 5): ?>
+    <div class="row">
+        <div class="col-lg-4 col-xs-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
+
+                <div class="info-box-content">
+                <span class="info-box-text">Number of Tutors</span>
+                <span class="info-box-number">100000</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
+
+                <div class="info-box-content">
+                <span class="info-box-text">Number of Tutees</span>
+                <span class="info-box-number">10000</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-book"></i></span>
+
+                <div class="info-box-content">
+                <span class="info-box-text">Subjects Taught</span>
+                <span class="info-box-number">1,410</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+        </div>    
+    </div>   
+
+    <div class="row">
+        <div class="col-lg-4 col-xs-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-commenting"></i></span>
+
+                <div class="info-box-content">
+                <span class="info-box-text">Subjects Requested to be Tutored</span>
+                <span class="info-box-number">100000</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-calendar-check-o"></i></span>
+
+                <div class="info-box-content">
+                <span class="info-box-text">Sessions for Today</span>
+                <span class="info-box-number">10000</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+        </div>
+   
+    </div>       
+
+    <div class="row">
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>
+                        <?php
+                            if($_SESSION['typeID'] == 5){
+                                echo $admin_sess;
+                            }
+                        ?>
+                    </h3>
+
+                    <p>All Tutorial Sessions</p>
+                </div>
+                    <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                    </a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3><?php
+                        if($_SESSION['typeID'] == 5){
+                            echo $admin_pend;
+                        }
+                        ?>
+                    </h3>
+
+                    <p>Pending Sessions</p>
+                </div>
+                    <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                    </a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3><?php
+                        if($_SESSION['typeID'] == 5){
+                            echo $admin_feedback;
+                        }
+                        ?>
+                    </h3>
+
+                    <p>Finished Feedbacks</p>
+                </div>
+                    <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">
+                    More info <i class="fa fa-arrow-circle-right"></i>
+                    </a>
+            </div>
         </div>
     </div>
     <?php endif; ?>
-    <!--admin tutsess box-->
 
-    <!--BOX2-->
-    <div class="col-lg-6">
-        <div class="list-group">
-            <li class="list-group-item list-group-item-warning">Feedback</li>
-            <a href="#" class="list-group-item">All Feedbacks<span class="badge">21</span></a>
-            <a href="#" class="list-group-item">Pending Feedbacks<span class="badge">21</span></a>
-            <a href="#" class="list-group-item">Answered Feedbacks<span class="badge">21</span></a>
-        </div>
-    </div>
-    <!--BOX2-->
-
-</div>
-</div>
