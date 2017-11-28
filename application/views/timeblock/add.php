59df9de61b4cad2f615d71b4475c11747b006819
biancapatrizavalmeo <biancapatriza@yahoo.com>
2017-11-29 01:03:50 +0800

    fixed timeblock add and edit format

--- application/views/timeblock/add.php
+++ application/views/timeblock/add.php
@@ -10,21 +10,22 @@
 					<div class="col-md-6">
 						<label for="timeStart" class="control-label"><span class="text-danger">*</span>TimeStart</label>
 						<div class="form-group">
-							<input type="text" name="timeStart" value="<?php echo $this->input->post('timeStart'); ?>" class="form-control" id="timeStart" />
+							<input type="time" name="timeStart" value="<?php echo $this->input->post('timeStart'); ?>" class="form-control" id="timeStart" />
 							<span class="text-danger"><?php echo form_error('timeStart');?></span>
 						</div>
 					</div>
 					<div class="col-md-6">
 						<label for="timeEnd" class="control-label"><span class="text-danger">*</span>TimeEnd</label>
 						<div class="form-group">
-							<input type="text" name="timeEnd" value="<?php echo $this->input->post('timeEnd'); ?>" class="form-control" id="timeEnd" />
+							<input type="time" name="timeEnd" value="<?php echo $this->input->post('timeEnd'); ?>" class="form-control" id="timeEnd" />
 							<span class="text-danger"><?php echo form_error('timeEnd');?></span>
 						</div>
 					</div>
 					<div class="col-md-6">
 						<label for="status" class="control-label"><span class="text-danger">*</span>Status</label>
 						<div class="form-group">
-							<input type="text" name="status" value="<?php echo $this->input->post('status'); ?>" class="form-control" id="status" />
+							<label class="radio-inline"><input type="radio" name="status" value="Available" class="radio" id="status" >Available</label>
+							<label class="radio-inline"><input type="radio" name="status" value="Not Available" class="radio" id="status" />Not Available</label>
 							<span class="text-danger"><?php echo form_error('status');?></span>
 						</div>
 					</div>
